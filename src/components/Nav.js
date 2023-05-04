a01f0cc73015f21df2d69adcdc7ec1a6d6b53941
Srishty Mangutte <68679980+Srish-ty@users.noreply.github.com>
2023-05-04 18:08:52 +0530

    Initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import "./styles/Nav.styles.css";
+
+export const Nav = () => {
+  return (
+    <nav>
+      <ul className="nfirst">
+        <li>Products</li>
+        <li>Solutions</li>
+        <li>Pricing</li>
+        <li>Resources</li>
+        <li>Company</li>
+      </ul>
+    </nav>
+  );
+};
